902750a9d656b9a57b28c89b8b93646773bbf333
chickenspaceprogram <pestpestthechicken@yahoo.com>
2024-04-05 14:58:10 -0700

    it's fricked. clueless on how to create an array that doesn't suck. also there is/was a bug where the true atk win chance was actually 1- the chance calculated. idk man,

--- scripts/main.js
+++ scripts/main.js
@@ -14,18 +14,14 @@ function main() {
 function makeTbl(atk, def) {
     tbl_size = [atk, def];
     atkWinProb = []
-    for (let i = 0; i <= atk; i++) {
-        let subarray = [];
-        for (let j = 0; j <= def; j++) {
-            subarray.push(0)
-        };
-        atkWinProb.push(subarray)
-    };
-    atkExpLoss = atkWinProb;
-    defExpLoss = atkWinProb;
-    for (let i = 1; i <= def; i++) {
-        atkWinProb[0][i] = 1
+    for (i = 0; i <= atk; i++) {
+        atkWinProb.push(JSON.parse(JSON.stringify(Array(def + 1).fill(0))))
+        console.log(atkWinProb)
     }
+    atkWinProb[0] = Array(def + 1).fill(1)
+    atkWinProb[0][0] = 0
+    atkWinProb[1][2] = 69
+    console.log(atkWinProb)
     /*
     Why did I think it was a good idea to change variable names from the python script?
     This sucked to rewrite for the FIFTH time. 
@@ -44,30 +40,30 @@ function makeTbl(atk, def) {
             if (numDef >= 2) {
                 if (numAtk >= 3) {
                     atkWinProb[numAtk][numDef] = atkWinProb[numAtk][numDef - 2] * (2890 / 7776) + atkWinProb[numAtk - 2][numDef] * (2275 / 7776) + atkWinProb[numAtk - 1][numDef - 1] * (2611 / 7776);
-                    atkExpLoss[numAtk][numDef] = atkExpLoss[numAtk][numDef - 2] * (2890 / 7776) + (atkExpLoss[numAtk - 2][numDef] + 2) * (2275 / 7776) + (atkExpLoss[numAtk - 1][numDef - 1] + 1) * (2611 / 7776);
-                    defExpLoss[numAtk][numDef] = (defExpLoss[numAtk][numDef - 2] + 2) * (2890 / 7776) + defExpLoss[numAtk - 2][numDef] * (2275 / 7776) + (defExpLoss[numAtk - 1][numDef - 1] + 1) * (2611 / 7776);
+                    //atkExpLoss[numAtk][numDef] = atkExpLoss[numAtk][numDef - 2] * (2890 / 7776) + (atkExpLoss[numAtk - 2][numDef] + 2) * (2275 / 7776) + (atkExpLoss[numAtk - 1][numDef - 1] + 1) * (2611 / 7776);
+                    //defExpLoss[numAtk][numDef] = (defExpLoss[numAtk][numDef - 2] + 2) * (2890 / 7776) + defExpLoss[numAtk - 2][numDef] * (2275 / 7776) + (defExpLoss[numAtk - 1][numDef - 1] + 1) * (2611 / 7776);
                 } else if (numAtk === 2) {
                     atkWinProb[numAtk][numDef] = atkWinProb[numAtk][numDef - 2] * (295 / 1296) + atkWinProb[numAtk - 2][numDef] * (581 / 1296) + atkWinProb[numAtk - 1][numDef - 1] * (420 / 1296);
-                    atkExpLoss[numAtk][numDef] = atkExpLoss[numAtk][numDef - 2] * (295 / 1296) + (atkExpLoss[numAtk - 2][numDef] + 2) * (581 / 1296) + (atkExpLoss[numAtk - 1][numDef - 1] + 1) * (420 / 1296);
-                    defExpLoss[numAtk][numDef] = (defExpLoss[numAtk][numDef - 2] + 2) * (295 / 1296) + defExpLoss[numAtk - 2][numDef] * (581 / 1296) + (defExpLoss[numAtk - 1][numDef - 1] + 1) * (420 / 1296);
+                    //atkExpLoss[numAtk][numDef] = atkExpLoss[numAtk][numDef - 2] * (295 / 1296) + (atkExpLoss[numAtk - 2][numDef] + 2) * (581 / 1296) + (atkExpLoss[numAtk - 1][numDef - 1] + 1) * (420 / 1296);
+                    //defExpLoss[numAtk][numDef] = (defExpLoss[numAtk][numDef - 2] + 2) * (295 / 1296) + defExpLoss[numAtk - 2][numDef] * (581 / 1296) + (defExpLoss[numAtk - 1][numDef - 1] + 1) * (420 / 1296);
                 } else {
                     atkWinProb[numAtk][numDef] = atkWinProb[numAtk][numDef - 1] * (55 / 216) + atkWinProb[numAtk - 1][numDef] * (161 / 216);
-                    atkExpLoss[numAtk][numDef] = atkExpLoss[numAtk][numDef - 1] * (55 / 216) + (atkExpLoss[numAtk - 1][numDef] + 1) * (161 / 216);
-                    defExpLoss[numAtk][numDef] = (defExpLoss[numAtk][numDef - 1] + 1) * (55 / 216) + defExpLoss[numAtk - 1][numDef] * (161 / 216);
+                    //atkExpLoss[numAtk][numDef] = atkExpLoss[numAtk][numDef - 1] * (55 / 216) + (atkExpLoss[numAtk - 1][numDef] + 1) * (161 / 216);
+                    //defExpLoss[numAtk][numDef] = (defExpLoss[numAtk][numDef - 1] + 1) * (55 / 216) + defExpLoss[numAtk - 1][numDef] * (161 / 216);
                 };
             } else {
                 if (numAtk >= 3) {
                     atkWinProb[numAtk][numDef] = atkWinProb[numAtk][numDef - 1] * (855 / 1296) + atkWinProb[numAtk - 1][numDef] * (441 / 1296);
-                    atkExpLoss[numAtk][numDef] = atkExpLoss[numAtk][numDef - 1] * (855 / 1296) + (atkExpLoss[numAtk - 1][numDef] + 1) * (441 / 1296);
-                    defExpLoss[numAtk][numDef] = (defExpLoss[numAtk][numDef - 1] + 1) * (855 / 1296) + defExpLoss[numAtk - 1][numDef] * (441 / 1296);
+                    //atkExpLoss[numAtk][numDef] = atkExpLoss[numAtk][numDef - 1] * (855 / 1296) + (atkExpLoss[numAtk - 1][numDef] + 1) * (441 / 1296);
+                    //defExpLoss[numAtk][numDef] = (defExpLoss[numAtk][numDef - 1] + 1) * (855 / 1296) + defExpLoss[numAtk - 1][numDef] * (441 / 1296);
                 } else if (numAtk === 2) {
                     atkWinProb[numAtk][numDef] = atkWinProb[numAtk][numDef - 1] * (125 / 216) + atkWinProb[numAtk - 1][numDef] * (91 / 216);
-                    atkExpLoss[numAtk][numDef] = atkExpLoss[numAtk][numDef - 1] * (125 / 216) + (atkExpLoss[numAtk - 1][numDef] + 1) * (91 / 216);
-                    defExpLoss[numAtk][numDef] = (defExpLoss[numAtk][numDef - 1] + 1) * (125 / 216) + defExpLoss[numAtk - 1][numDef] * (91 / 216);
+                    //atkExpLoss[numAtk][numDef] = atkExpLoss[numAtk][numDef - 1] * (125 / 216) + (atkExpLoss[numAtk - 1][numDef] + 1) * (91 / 216);
+                    //defExpLoss[numAtk][numDef] = (defExpLoss[numAtk][numDef - 1] + 1) * (125 / 216) + defExpLoss[numAtk - 1][numDef] * (91 / 216);
                 } else {
                     atkWinProb[numAtk][numDef] = atkWinProb[numAtk][numDef - 1] * (15 / 36) + atkWinProb[numAtk - 1][numDef] * (21 / 36);
-                    atkExpLoss[numAtk][numDef] = atkExpLoss[numAtk][numDef - 1] * (15 / 36) + (atkExpLoss[numAtk - 1][numDef] + 1) * (21 / 36);
-                    defExpLoss[numAtk][numDef] = (defExpLoss[numAtk][numDef - 1] + 1) * (15 / 36) + defExpLoss[numAtk - 1][numDef] * (21 / 36);
+                    //atkExpLoss[numAtk][numDef] = atkExpLoss[numAtk][numDef - 1] * (15 / 36) + (atkExpLoss[numAtk - 1][numDef] + 1) * (21 / 36);
+                    //defExpLoss[numAtk][numDef] = (defExpLoss[numAtk][numDef - 1] + 1) * (15 / 36) + defExpLoss[numAtk - 1][numDef] * (21 / 36);
                 };
             };
         };
